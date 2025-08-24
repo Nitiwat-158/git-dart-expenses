f83057d65cec5e8e3e98015071c54348b8135cda
Nitiwat-158 <6631501158@lamduan.mfu.ac.th>
2025-08-24 16:50:42 +0700

    initial commit

---
+++ dart_expense_tracking/bin/dart_expense_tracking.dart
@@ -0,0 +1,5 @@
+import 'package:dart_expense_tracking/dart_expense_tracking.dart' as dart_expense_tracking;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_expense_tracking.calculate()}!');
+}
---
+++ dart_expense_tracking/test/dart_expense_tracking_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_expense_tracking/dart_expense_tracking.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
