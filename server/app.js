f71f9d23380ecc1e2d1989864a2386f5c7f4b2f6
Kanphong <6631501148@lamduan.mfu.ac.th>
2025-08-25 23:13:18 +0700

    edits db.js and app.js

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const con = require('./db');
+
+app.get('/expense', (_req, res) => {
+    const sql = "SELECT * FROM expense";
+    con.query(sql, function(err, results) {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        res.json(results);
+    })
+});
+
+
+// ---------- Server starts here ---------
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is running at ' + PORT);
+});
+
