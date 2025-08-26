cbcb581afe9b5a43cc49359f375f04fbf3a02c64
Nitiwat-158 <6631501158@lamduan.mfu.ac.th>
2025-08-27 00:05:32 +0700

    Fix login

--- server/app.js
+++ server/app.js
@@ -5,6 +5,30 @@ const con = require('./db');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// login
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    const sql = "SELECT id, password FROM user WHERE username = ?";
+    con.query(sql, [username], function(err, results) {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        if(results.length != 1) {
+            return res.status(401).send("Wrong username");
+        }
+        // compare passwords
+        bcrypt.compare(password, results[0].password, function(err, same) {
+            if(err) {
+                return res.status(500).send("Hashing error");
+            }
+            if(same) {
+                return res.json({ message: "Login OK", userId: results[0].id });
+            }
+            return res.status(401).send("Wrong password");
+        });
+    })
+});
+
 // 1. Show all expenses
 app.get('/expenses', (_req, res) => {
     const sql = "SELECT * FROM expense";
